6c677804c2d1f63d08e0106e2f71ecda710cf3ee
Tyler Wright <tyler.wright@fullstackacademy.com>
2024-04-15 09:12:15 -0700

    Body Parser

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 require('dotenv').config()
 const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require('body-parser');
 const { client } = require("./db");
 
 const app = express();
 const PORT = 8080;
 
 client.connect();
+app.use(morgan('dev'));
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+  // Log out everything on the body of the request
+  console.log("<___BODY LOGGER START_____>");
+  console.log(req.body);
+  console.log("<___BODY LOGGER END_______>");
+  next();
+})
 
 app.get("/", (req, res) => {
   res.send(`<h1>Comic Store</h1>`);
